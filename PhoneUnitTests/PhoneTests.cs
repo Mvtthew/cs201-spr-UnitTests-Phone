f16be8a65cb76ce5496a53d9ecf6317130b9fd67
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-01-23 21:03:53 +0100

    :heart: Done

---
+++ PhoneUnitTests/PhoneTests.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace PhoneUnitTests
+{
+    [TestClass]
+    public class PhoneTests
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            Assert.AreEqual(p.Owner, o);
+            Assert.AreEqual(p.PhoneNumber, pn);          
+        }
+
+        [TestMethod]
+        public void PhoneOwnerTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            Assert.AreEqual(p.Owner, o);
+        }
+        
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void PhoneOwnerOwnerNameExceptionTest()
+        {
+            string o = "";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            string owner = p.Owner;
+        }
+
+        [TestMethod]
+        public void PhoneNumberTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            Assert.AreEqual(p.PhoneNumber, pn);
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void PhoneNumberPhoneNumberIsEmptyExceptionTest()
+        {
+            string o = "Mateusz";
+            string pn = "";
+            Phone p = new Phone(o, pn);
+
+            string phoneNumber = p.PhoneNumber;
+        }
+        
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void PhoneNumberInvalidPhoneNumberExceptionTest()
+        {
+            string o = "Mateusz";
+            string pn = "23";
+            Phone p = new Phone(o, pn);
+
+            string phoneNumber = p.PhoneNumber;
+        }
+        
+        [TestMethod]
+        public void PhoneBookCountTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            Assert.AreEqual(p.Count, 0);
+        }
+        
+        [TestMethod]
+        public void PhoneBookAddContactTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            string co = "Sw Marcellina";
+            string cpn = "777777777";
+
+            p.AddContact(co, cpn);
+
+            Assert.AreEqual(p.Count, 1);
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void PhoneBookAddContactPhoneBookFullExceptionTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            for (int i = 0; i < 102; i++)
+            {
+                string co = "Sw Marcellina" + i;
+                string cpn = (100000000 + i).ToString();
+
+                p.AddContact(co, cpn);
+            }
+        }
+
+        [TestMethod]
+        public void PhoneCallTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            string co = "Sw Marcellina";
+            string cpn = "777777777";
+
+            p.AddContact(co, cpn);
+
+            string callMessage = p.Call(co);
+
+            Assert.IsTrue(callMessage.Contains(co));
+        }
+
+        [ExpectedException(typeof(InvalidOperationException))]
+        [TestMethod]
+        public void PhoneCallNotAddedPersonToPhoneBookExceptionTest()
+        {
+            string o = "Mateusz";
+            string pn = "123321231";
+            Phone p = new Phone(o, pn);
+
+            string co = "Sw Marcellina";
+            string cpn = "777777777";
+
+            string callMessage = p.Call(co);
+        }
+    }
+}
---
+++ PhoneUnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PhoneUnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PhoneUnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc407f19-8f0d-4a4b-a439-8aca78ee6530")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
